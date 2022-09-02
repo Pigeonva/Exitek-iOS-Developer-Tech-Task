b7bd9a4a350d513d83f7130577b73a6d126d4d09
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-09-02 09:43:06 +0300

    Initial commit

---
+++ Exitek iOS Developer Tech Task.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+// Implement mobile phone storage protocol
+// Requirements:
+// - Mobiles must be unique (IMEI is an unique number)
+// - Mobiles must be stored in memory
+
+import UIKit
+
+protocol MobileStorage {
+func getAll() -> Set<Mobile>
+func findByImei(_ imei: String) -> Mobile?
+func save(_ mobile: Mobile) throws -> Mobile
+func delete(_ product: Mobile) throws
+func exists(_ product: Mobile) -> Bool
+}
+
+struct Mobile: Hashable {
+let imei: String
+let model: String
+}
