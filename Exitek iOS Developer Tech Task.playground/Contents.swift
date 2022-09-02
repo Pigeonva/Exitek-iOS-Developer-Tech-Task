e84c1f78ef334de27e5bb0445dd8335c47a1fe58
Pigeonva <77341488+Pigeonva@users.noreply.github.com>
2022-09-02 12:15:16 +0300

    end with tasks

--- Exitek iOS Developer Tech Task.playground/Contents.swift
+++ Exitek iOS Developer Tech Task.playground/Contents.swift
@@ -7,14 +7,68 @@
 import UIKit
 
 protocol MobileStorage {
-func getAll() -> Set<Mobile>
-func findByImei(_ imei: String) -> Mobile?
-func save(_ mobile: Mobile) throws -> Mobile
-func delete(_ product: Mobile) throws
-func exists(_ product: Mobile) -> Bool
+    func getAll() -> Set<Mobile>
+    func findByImei(_ imei: String) -> Mobile?
+    func save(_ mobile: Mobile) throws -> Mobile
+    func delete(_ product: Mobile) throws
+    func exists(_ product: Mobile) -> Bool
 }
 
 struct Mobile: Hashable {
-let imei: String
-let model: String
+    let imei: String
+    let model: String
 }
+
+enum MobileDataBaseError: Error {
+    
+    case databaseHasThisMobile
+    case databaseDoesntHaveThisMobile
+}
+
+class MobilePhoneDataBase: MobileStorage {
+    
+    var mobilePhones = [String : Mobile]()
+    
+    func getAll() -> Set<Mobile> {
+        var set: Set<Mobile> = []
+        
+        mobilePhones.forEach { key, value in
+            set.insert(value)
+        }
+        return set
+    }
+    
+    func findByImei(_ imei: String) -> Mobile? {
+        if mobilePhones[imei] != nil {
+            return mobilePhones[imei]
+        } else {
+            return nil
+        }
+    }
+    
+    func save(_ mobile: Mobile) throws -> Mobile {
+        if !exists(mobile) {
+            mobilePhones.updateValue(mobile, forKey: mobile.imei)
+        } else {
+            throw MobileDataBaseError.databaseHasThisMobile
+        }
+        return mobile
+    }
+    
+    func delete(_ product: Mobile) throws {
+        if exists(product) {
+            mobilePhones[product.imei] = nil
+        } else {
+            throw MobileDataBaseError.databaseDoesntHaveThisMobile
+        }
+    }
+    
+    func exists(_ product: Mobile) -> Bool {
+        if mobilePhones[product.imei] != nil && mobilePhones[product.imei]?.model == product.model {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
